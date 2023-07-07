2276d7aacd93deb7dc2f4fd3cafd2f2ea45913fd
Yu Shi <shiyu_k1994@qq.com>
2023-07-07 06:21:53 +0000

    initial framework for dataset

---
+++ src/data/text.py
@@ -0,0 +1,34 @@
+from dataset import Data
+from torch import LongTensor
+import numpy as np
+from typing import Union, List
+from transformers import PreTrainedTokenizer
+from molecule import Molecule
+
+from src.data.text import TOKEN_SEQ_T
+
+TOKEN_SEQ_T = Union[LongTensor, np.array]
+MOL_POS_T = Union[LongTensor, List[int], np.array]
+
+class Text(Data):
+    def __init__(self, token_seq: TOKEN_SEQ_T, **kwargs) -> None:
+        super().__init__()
+        self.token_seq = token_seq
+        for key, value in kwargs:
+            self.__dict__[key] = value
+
+    def from_raw_text(cls, text: str, tokenizer: PreTrainedTokenizer) -> 'Text':
+        tokenized = tokenizer.tokenize(text=text)
+        token_seq = tokenized.input_ids[0]
+        return cls(token_seq=token_seq)
+
+class MixedText(Text):
+    def __init__(self, token_seq: TOKEN_SEQ_T,
+                 mol_poses: MOL_POS_T = None,
+                 molecules: List[Molecule] = None,
+                 **kwargs) -> None:
+        super().__init__(token_seq, **kwargs)
+        assert len(mol_poses) == len(molecules), f"Numbers of molecules in mol_poses and molecules do not match."
+
+    def from_json(cls, json_text: str, tokenizer: PreTrainedTokenizer) -> 'Text':
+        raise NotImplementedError
