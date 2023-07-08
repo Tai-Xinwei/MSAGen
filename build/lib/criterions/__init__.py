26ea958f20f3aa5d3981345cfa62aeef6b03e2f2
Peiran Jin <peiranjin@microsoft.com>
2023-07-08 02:31:06 +0000

    remove build

--- build/lib/criterions/__init__.py
+++
@@ -1,3 +0,0 @@
-# from . import graph_prediction
-# from . import binary_logloss
-from . import mae3d
--- build/lib/modules/__init__.py
+++
@@ -1,5 +0,0 @@
-from .graphormer_sentence_encoder import (
-    GraphormerSentenceEncoder,
-    Node_decoder,
-    init_bert_params,
-)
