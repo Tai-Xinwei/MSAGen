d81cf1b36c9a7479cf394eddfc24d80091513dac
shufang <shufang>
2023-07-12 08:26:09 +0000

    simple trainer (Work in progress)

--- sfm/pipeline/trainer.py
+++ sfm/pipeline/trainer.py
@@ -5,14 +5,15 @@ import deepspeed
 import torch
 import torch.nn as nn
 from torch.optim import Optimizer
-from torch.optim.lr_scheduler import _LRScheduler
-from torch.utils.data import Dataset
-
+from torch.optim.lr_scheduler import LRScheduler
+from torch.utils.data import Dataset, DataLoader
+from abc import ABC, abstractmethod
 import numpy as np
 import random
-from abc import ABC, abstractmethod
 from dataclasses import dataclass
 from enum import Enum
+
+from tqdm import tqdm
 
 
 class TraingStrategy(Enum):
@@ -27,164 +28,109 @@ class TrainerConfig:
     epochs: int = 100
     seed: int = 46
     loss_scale: float = 1.0
-
-class EntityType(Enum):
-    Text = 0
-    Mol = 1
-    Protein = 2
-
-@dataclass
-class DataBatch:
-    # Batch size * sequence length * hidden size
-    x: Optional[torch.Tensor] = None
+    batch_size: int = 32
     
-    # the entity mask dict form entity type (e.g., moleclue) to a list of masks
-    # Each encoder can only process the entities they care about
-    mask: Optional[Dict[EntityType, List[torch.Tensor]]] = None
-    
-    # the label of the batch, Batch size * sequence length
-    label: Optional[torch.Tensor] = None
+    ## Distibuated training
+    global_rank: int = 0
+    local_rank: int = 0
+    node_rank: int = 0
+    world_size: int = 1
+    num_nodes: int = 1
 
 
-class Trainer(ABC):
+class Model(nn.Module, ABC):
     @abstractmethod
-    def train(self):
+    def compute_loss(self, pred, batch):
         pass
-    
-    @abstractmethod
-    def build_model(self) -> nn.Module:
-        pass
-    
-    @abstractmethod
-    def save_checkpoint(self):
-        pass
-    
-    @abstractmethod
-    def build_optimizer(self, parameters: Iterator[nn.Parameter]) -> Optimizer:
-        pass
-    
-    @abstractmethod
-    def build_scheduler(self, optimizer: Optimizer) -> _LRScheduler:
-        pass
-    
-    @abstractmethod
-    def build_criterion(self):
-        pass
-    
-    @abstractmethod
-    def load_checkpoint(self):
-        pass  
 
-    @abstractmethod
-    def build_trainig_data_loader(self):
-        pass
-    
-    @abstractmethod
-    def build_validation_data_loader(self):
-        pass
-    
 
-class BaseTrainer(Trainer):
+def seed_everything(seed):
+    torch.cuda.manual_seed_all(seed)
+    torch.manual_seed(seed)
+    np.random.seed(seed)
+    random.seed(seed)
+
+class Trainer(object):
     def __init__(
         self,
-        args: TrainerConfig
+        args: TrainerConfig,
+        model: Model,
+        train_data: Dataset,
+        valid_data: Optional[Dataset] = None,
+        test_data: Optional[Dataset] = None,
     ):
         super().__init__()
         self.args = args
 
-        self.net = self.build_model()
-        self.creterion = self.build_criterion()
-        self.optimizer = self.build_optimizer(self.net.parameters())
-        self.scheduler = self.build_scheduler(self.optimizer)
+        self.model = model
+        self.train_data = train_data
+        self.valid_data = valid_data
+        self.test_data = test_data
         
+        self.optimizer = self.build_optimizer(self.model.parameters())
+        self.lr_scheduler = self.build_lr_scheduler(self.optimizer)
         
-        self.train_data_loader = self.build_trainig_data_loader()
-        self.valid_data_loader = self.build_validation_data_loader()
-        
-        train_data = self.train_data_loader.load_data()
-        
-        self.model_engine, _, self.train_loader, _ = deepspeed.initialize(
-            args=args,
-            model=self.net,
-            model_parameters=self.net,
-            training_data=train_data,
-            collate_fn=train_data.collater2,
-            optimizer=self.optimizer,
-            lr_scheduler=self.scheduler
-        )
-        
-        # define model
-        # net = GraphormerModel(args)
-        # count_paranum(net)
-
-        # define criterion
-        # self.L1loss = L1_criterions(args, reduction='mean', data_mean=data_mean, data_std=data_std)
-
-        # define optimizer
-        # parameters = filter(lambda p: p.requires_grad, net.parameters())
-        # self.optimizer = optim.Adam(parameters, lr=args.lr, weight_decay=args.weight_decay)
-
-        # define scheduler
-        # self.scheduler = optim.lr_scheduler.CosineAnnealingLR(self.optimizer, T_max=args.epochs, eta_min=0.0)
-
-        # define model engine
-        # self.model_engine, _, self.train_loader, _ = deepspeed.initialize(args=args,
-        #                                                                   model=net,
-        #                                                                   model_parameters=parameters,
-        #                                                                   training_data=train_data,
-        #                                                                   collate_fn=train_data.collater2,
-        #                                                                   optimizer=self.optimizer,
-        #                                                                   lr_scheduler=self.scheduler,
-        #                                                                   loss_scale=args.loss_scale,
-        #                                                                   loss_fn=self.L1loss,
-        #                                                                   )
-
-        # define dataloader
-        # len_val, self.val_dataloader = self.set_dataloader(data=val_data)
-        # len_test, self.test_dataloader = self.set_dataloader(data=test_data)
-
-        # load checkpoints
-        # self.load_checkpoint()
-        
-    def seed_everything(self, seed):
-        torch.cuda.manual_seed_all(seed)
-        torch.manual_seed(seed)
-        np.random.seed(seed)
-        random.seed(seed)
-        
+        self.train_data_loader = self.build_data_loader(train_data, shuffle=True, collater=train_data.collater)
+        self.valid_data_loader = self.build_data_loader(valid_data, shuffle=False, collater=valid_data.collater)
+    
+    def build_optimizer(self, parameters) -> Optimizer:
+        return torch.optim.Adam(parameters, lr=1e-3) # todo: add more options
+    
+    def build_lr_scheduler(self, optimizer) -> LRScheduler:
+        return torch.optim.lr_scheduler.StepLR(optimizer, step_size=10, gamma=0.1) # todo: add more options
         
     def load_checkpoint(self, checkpoint_path=None):
         pass
-
-    def set_dataloader(self, data=None):
-        len_data = 0
-        if data is not None:
-            len_data = len(data)
-            validsampler = torch.utils.data.distributed.DistributedSampler(
-                data, num_replicas=self.model_engine.dp_world_size, shuffle=True
+    
+    def build_data_loader(self, data: Optional[Dataset], shuffle: bool, collater) -> Optional[DataLoader]:
+        if data is None:
+            return None
+        
+        if self.args.world_size == 1:
+            sampler = torch.utils.data.RandomSampler(data) if shuffle else None
+        else:
+            sampler = torch.utils.data.distributed.DistributedSampler(
+                data, num_replicas=self.args.world_size, shuffle=shuffle
             )
-            dataloader = torch.utils.data.DataLoader(
-                data,
-                sampler=validsampler,
-                batch_size=self.model_engine.train_micro_batch_size_per_gpu(),
-                collate_fn=data.collater,
-            )
-
-        return len_data, dataloader
+        
+        return DataLoader(
+            data,
+            sampler=sampler,
+            batch_size=self.args.batch_size,
+            collate_fn=collater,
+        )  
 
     def train(self):
         print("start training")
-        self.seed_everything(self.args.seed)
+        seed_everything(self.args.seed)
+        
+        assert self.train_data_loader is not None
         
         # TODO: add more logging
         for epoch in range(self.args.epochs):
-            for i, batch_data in enumerate(self.train_data_loader):
-                model_output = self.model_engine.train_batch(batch_data)
-                loss = self.creterion(model_output, batch_data.label)
-                self.model_engine.backward(loss)
-                self.model_engine.step()
+            print("epoch: ", epoch)
+            for i, batch_data in tqdm(enumerate(self.train_data_loader)):
+                # TODO: change to accelerator
+                pred = self.model(batch_data)
+                loss = self.model.compute_loss(pred, batch_data)
+                self.optimizer.zero_grad()
+                loss.backward()
+                self.optimizer.step()
+                self.lr_scheduler.step()
                 
                 
-                
-                
-            
+
+    def validate(self):
+        if self.valid_data_loader is None:
+            print("No validation data, skip validation")
+            return
+        
+        print("start validation")
+
+        #TODO: support multiple losses
+        for i, batch_data in tqdm(enumerate(self.valid_data_loader)):
+            loss = self.model(batch_data)
+            print("loss: ", loss)
+        
+    
+    
---
+++ tests/test_trainer.py
@@ -0,0 +1,42 @@
+from sfm.pipeline.trainer import Trainer, TrainerConfig, Model
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class DummyNN(Model):
+    def __init__(self):
+        super().__init__()
+        self.fc = nn.Linear(10, 10)
+    
+    def forward(self, x):
+        return self.fc(x)
+    
+    def compute_loss(self, pred, batch):
+        return F.l1_loss(pred, batch)
+
+class DummyDataset(torch.utils.data.Dataset):
+    def __init__(self):
+        super().__init__()
+        self.data = torch.randn(5, 8, 10)
+    
+    def __getitem__(self, index):
+        return self.data[index]
+    
+    def __len__(self):
+        return len(self.data)
+    
+    def collater(self, batch):
+        return torch.stack(batch)
+
+
+def test_trainer():
+    config = TrainerConfig()
+    print(config)
+    
+    model = DummyNN()
+    train_data = DummyDataset()
+    valid_data = DummyDataset()
+    test_data = DummyDataset()
+    
+    trainer = Trainer(config, model, train_data, valid_data, test_data)
+    trainer.train()
