4c23300eb6d3924d99986cd0b1bfe3cb3a459218
shufang <shufang>
2023-07-13 05:58:13 +0000

    add more trainer code

--- tests/test_trainer.py
+++ tests/test_trainer.py
@@ -1,8 +1,12 @@
-from sfm.pipeline.trainer import Trainer, TrainerConfig, Model
+from sfm.pipeline.trainer import Trainer, TrainerConfig, Model, ModelOutput
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import math
+from torch.optim import Optimizer
+from torch.optim.lr_scheduler import LRScheduler
+from torch.utils.data.dataset import Dataset
+
+import tempfile
 
 class DummyNN(Model):
     def __init__(self):
@@ -12,10 +16,17 @@ class DummyNN(Model):
     def forward(self, x):
         return self.fc(x)
     
-    def compute_loss(self, pred, batch):
-        return F.l1_loss(pred, batch)
+    def compute_loss(self, pred, batch) -> ModelOutput:
+        loss = F.l1_loss(pred, batch)
+        return ModelOutput(loss=loss, log_output={'l2': F.mse_loss(pred, batch)})
 
-class DummyDataset(torch.utils.data.Dataset):
+    def config_optimizer(self) -> tuple[Optimizer, LRScheduler]:
+        optimizer =  torch.optim.Adam(self.parameters(), lr=1e-3)
+        lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, 1, gamma=0.1)
+        
+        return optimizer, lr_scheduler
+
+class DummyDataset(Dataset):
     def __init__(self):
         super().__init__()
         self.data = torch.randn(128, 10)
@@ -29,20 +40,19 @@ class DummyDataset(torch.utils.data.Dataset):
 def collater(batch):
     return torch.stack(batch)
 
+
 def test_trainer():
-    config = TrainerConfig(
-        lr_lambda=lambda epoch: 1.0/4000 if epoch < 4000 else 1.0/math.sqrt(epoch),
-        optimizer_args={
-            'betas': (0.9, 0.98),
-        },
-        epochs=1,
-    )
-    print(config)
-    
-    model = DummyNN()
-    train_data = DummyDataset()
-    valid_data = DummyDataset()
-    test_data = DummyDataset()
-    
-    trainer = Trainer(config, model=model, collater=collater, train_data=train_data, valid_data=valid_data, test_data=test_data)
-    trainer.train()
+    with tempfile.TemporaryDirectory() as save_dir:
+        config = TrainerConfig(
+            epochs=1,
+            save_dir=save_dir
+        )
+        print(config)
+        
+        model = DummyNN()
+        train_data = DummyDataset()
+        valid_data = DummyDataset()
+        test_data = DummyDataset()
+        
+        trainer = Trainer(config, model=model, collater=collater, train_data=train_data, valid_data=valid_data, test_data=test_data)
+        trainer.train()
