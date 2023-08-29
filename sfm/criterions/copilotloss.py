f0e4513d315eb3199156f361d626048aa2ffbc8c
Peiran Jin <peiranjin@microsoft.com>
2023-08-29 17:34:35 +0000

    minor chagne

--- sfm/criterions/copilotloss.py
+++ sfm/criterions/copilotloss.py
@@ -8,6 +8,8 @@ import torch.nn as nn
 
 from sfm.logging import logger
 
+import wandb  # isort:skip
+
 
 class CopilotCriterions(nn.Module):
     def __init__(self, config, vocab_size=32001, reduction="mean") -> None:
@@ -56,6 +58,11 @@ class CopilotCriterionsNumPP(CopilotCriterions):
     def __init__(self, config, vocab_size=32001, reduction="mean") -> None:
         super().__init__(config, vocab_size, reduction)
         self.l_num = nn.L1Loss(reduction=reduction)
+        self.l_bce = nn.BCEWithLogitsLoss(reduction=reduction)
+        self.global_step = 0
+        # TODO
+        self.mlp_bce = False
+        self.wandb_log = True
 
     def forward(self, output, label):
         labels = label[0].to(torch.int64)
@@ -76,11 +83,43 @@ class CopilotCriterionsNumPP(CopilotCriterions):
         loss = self.l1(shift_logits, shift_labels)
 
         num_idx = num_labels != -100
-        num_labels = num_labels[num_idx]
-        num_logits = num_logits[num_idx].view(-1)
+        if num_idx.any():
+            num_labels = num_labels[num_idx]
+            num_logits = num_logits[num_idx].view(-1)
+            num_loss = self.l_num(num_logits, num_labels)
+        else:
+            num_loss = torch.tensor(0.0).to(loss.device)
 
-        num_loss = self.l_num(num_logits, num_labels)
+        if self.mlp_bce:
+            lm_binary_label_mask = (labels == 8241) | (labels == 3782)
+            # left shift
+            lm_binary_label_mask_shift = torch.cat(
+                [
+                    lm_binary_label_mask[:, 1:],
+                    torch.zeros_like(lm_binary_label_mask[:, 0]).unsqueeze(-1),
+                ],
+                dim=-1,
+            )
+            lm_binary_logits = num_logits[lm_binary_label_mask_shift]
+            lm_binary_label = torch.zeros_like(lm_binary_logits)
+            lm_binary_label_dict_idx = labels[lm_binary_label_mask]
+            lm_binary_label[lm_binary_label_dict_idx == 8241] = 1
+            lm_binary_label[lm_binary_label_dict_idx == 3782] = 0
+            lm_binary_loss = self.l_bce(lm_binary_logits, lm_binary_label)
+        else:
+            lm_binary_loss = torch.tensor(0.0).to(loss.device)
 
-        logger.info(f"lm_loss, {loss}, num_loss, {num_loss}")
+        # For local single A100 training
+        self.global_step += 1
 
-        return loss + num_loss
+        if self.global_step % 10 == 0:
+            logger.info(
+                f"lm_loss, {loss}, num_loss, {num_loss} bce_loss, {lm_binary_loss}"
+            )
+            # # TODO: this should use wandb inside, check pfm/pretrain_pfm.sh
+            # if self.wandb_log:
+            #     wandb.log(
+            #         {"lm_loss": loss, "num_loss": num_loss, "bce_loss": lm_binary_loss}
+            #     )
+
+        return loss + num_loss + lm_binary_loss
