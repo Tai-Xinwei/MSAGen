50883356f760f0e4528f7881454f49d24276d674
peiranjin <peiranjin@microsoft.com>
2024-03-28 05:51:15 -0700

    add framework of psm, update readme

---
+++ sfm/models/psm/modules/invariant_encoder.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
+import torch.nn as nn
+
+
+class PSMEncoder(nn.Module):
+    def __init__(self):
+        super(PSMEncoder, self).__init__()
+
+    def forward(self, x):
+        pass
