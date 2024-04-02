568b94b75a7d97e7ab54354bc3648b978ade1735
v-lijunzhe <v-lijunzhe@microsoft.com>
2024-04-02 06:57:13 +0000

    Fix: fixed a bug in wandb that might not work when exporting new environment variables.

--- amlt/tox/run_local_withpde_toxpde_q.sh
+++ amlt/tox/run_local_withpde_toxpde_q.sh
@@ -1,56 +1,82 @@
 #!/bin/bash
 
-# Create the output directory
-mkdir -p /blob/pfmexp/output
-mkdir -p /blob/pfmexp/output/pfmdiff100M768_prob1522_m5_bs256_ddpmnoise_v1_pi_withpde_score
-mkdir -p /blob/pfmexp/output/pfmdiff100M768_prob1522_m5_bs256_ddpmnoise_v1_pi_withpde_score/checkpoints
-mkdir -p ./output
 
-# Export the environment variables
-export path=run.sh
+# Create the output father directory
+checkpoint_dir_path=/blob/pfmexp/output/junzhe/checkpoints
+mkdir -p ${checkpoint_dir_path}s
+
+# variables for distributed
+export pipeline_model_parallel_size=0 
+export strategy=DDP # option:(Single, DDP, Zero1, Zero2, Zero3, Pipeline, ThreeD)
+export launcher='openmpi'
+export hostfile='/job/hostfile'
+export MASTER_PORT=62347
+export MASTER_ADDR=127.0.0.1
+export OMPI_COMM_WORLD_SIZE=1
+
+# variables for optimizer
+export gradient_accumulation_steps=4
+export max_lr=1e-4
+export weight_decay=0.0
+
+# variables for batch size
+export max_tokens=3000
+export train_batch_size=1024
+export val_batch_size=1024
+
+# variables for log
+export log_interval=20
+
+# varibales for training
+export epochs=1000
+export total_num_steps=2000000
+export warmup_num_steps=1000
+export save_epoch_interval=1
+export save_batch_interval=10000000
+export data_path=/blob/data/afdb/48organisms-fullatom.lmdb/
+export dataset_name="."
+export save_dir="${checkpoint_dir_path}/${wandb_project}/${wandb_group}/${WANDB_RUN_NAME}"
+export loadcheck_path='.'
+# export loadcheck_path="${checkpoint_dir_path}/${wandb_project}/${wandb_group}/${WANDB_RUN_NAME}"
+
+# variables for wandb
+export WANDB_GROUP=pde_q_sample
+export wandb_team=junzhe_personal
+export WANDB_PROJECT=pde_running_loss
+export WANDB_RUN_NAME="without_pde"
+
+
+# variables for model
 export layers=12
-export num_pred_attn_layer=2
 export hidden_size=768
 export ffn_size=3072
 export num_head=32
-export num_3d_bias_kernel=8
+export num_pred_attn_layer=2
 export atom_loss_coeff=1.0
 export pos_loss_coeff=1.0
-export sandwich_ln="true"
+export max_length=256
 export dropout=0.0
 export attn_dropout=0.1
 export act_dropout=0.1
-export weight_decay=0.0
+export sandwich_ln=true
 export droppath_prob=0.0
-export noise_mode=diff
 export noise_scale=0.2
-export mask_ratio=0.5
-export mode_prob=0.1,0.5,0.2,0.2
-export d_tilde=1.0
-export max_lr=1e-4
-export strategy=DDP
-export pipeline_model_parallel_size=0
-export total_num_steps=2000000
-export warmup_num_steps=1000
-export train_batch_size=512 # 1024
-export val_batch_size=512 # 1024
-export max_tokens=4000
-export max_length=128
-export gradient_accumulation_steps=4
-export lamb_pde_q=0.01
+export noise_mode=diff
+export lamb_pde_q=0
 export lamb_pde_control=0
 export diffmode=score
-export wandb_group=tox_pde
-export wandb_project=pdediffusion
-export WANDB_RUN_NAME="withPDE_control_q"
-export log_interval=100
-export OMPI_COMM_WORLD_SIZE=1
-export loadcheck_path=/blob/pfmexp/output/pfmdiff100M768_prob1522_m5_bs256_ddpmnoise_v1_pi_withpde_score/checkpoints
-export data_path=/blob/data/afdb/48organism1m.lmdb/
-export save_dir=/blob/pfmexp/output/pfmdiff100M768_prob1522_m5_bs256_ddpmnoise_v1_pi_withpde_score/checkpoints
+# export seq_masking_method=continuousMask
+export seq_masking_method=transformerM
+export mask_ratio=0.5
+export d_tilde=1
+export mode_prob='0.1,0.2,0.6,0.1'
+export add_3d=true
+export no_2d=false
 
-# Create a temporary data directory
-mkdir -p /tmp/data/pm6-86m-3d-filter
+
+# Create the output directory
+mkdir -p ${loadcheck_path}
+mkdir -p ${save_dir}
 
 eval "$(conda shell.bash hook)"
 conda activate sfm
