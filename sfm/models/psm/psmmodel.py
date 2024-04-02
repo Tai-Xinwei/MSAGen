652e86b463ecca4bfb715faedf595adea841d1eb
Peiran Jin <peiranjin@microsoft.com>
2024-04-02 03:22:33 +0000

    Merged PR 268: update psm

--- sfm/models/psm/psmmodel.py
+++ sfm/models/psm/psmmodel.py
@@ -13,8 +13,9 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 from sfm.logging import logger
-from sfm.models.psm.modules.equivariant import EquivariantDecoder
-from sfm.models.psm.modules.invariant_encoder import PSMEncoder
+from sfm.models.psm.equivariant.equivariant import EquivariantDecoder
+from sfm.models.psm.invariant.invariant_encoder import PSMEncoder
+from sfm.models.psm.modules.embedding import PSMMixEmbedding
 from sfm.models.psm.psm_config import PSMConfig
 from sfm.modules.get_activation_fn import get_activation_fn
 from sfm.modules.layer_norm import LayerNorm
@@ -175,6 +176,9 @@ class PSM(nn.Module):
         super().__init__()
         self.max_positions = args.max_positions
         self.args = args
+
+        # Implement the embedding
+        self.embedding = PSMMixEmbedding(psm_config)
 
         # Implement the encoder
         self.encoder = PSMEncoder(psm_config)
