17f4f8881a9a828789390c44540e00c098604cac
peiranjin <peiranjin@microsoft.com>
2024-05-03 07:08:45 -0700

    save

--- sfm/tasks/tox/sample_tox.py
+++ sfm/tasks/tox/sample_tox.py
@@ -75,7 +75,7 @@ def load_data(args):
 
 
 def load_model(args):
-    model = TOXModel(args, loss_fn=ProteinMAE3dCriterions, load_ckpt=False).cuda()
+    model = TOXModel(args, loss_fn=ProteinMAE3dCriterions, load_ckpt=True).cuda()
     logger.info(f"load model from {args.loadcheck_path}")
 
     model.eval()
