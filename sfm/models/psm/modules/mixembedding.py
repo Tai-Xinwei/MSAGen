351d6c5907b77c3adb1c997cffc0a1e0f09537d0
peiranjin <peiranjin@microsoft.com>
2024-07-10 19:12:27 -0700

    Save

--- sfm/models/psm/modules/mixembedding.py
+++ sfm/models/psm/modules/mixembedding.py
@@ -141,8 +141,8 @@ class PSMMix3dEmbedding(nn.Module):
             )
 
             # time raito is 0 at time step == 0, time raito is 1 at time step >= 0.05, linear increase between 0 and 0.05
-            time_ratio = torch.clamp(time_step / 0.05, 0.0, 1.0)
-            x += atom_feature_embedding * time_ratio.unsqueeze(-1)
+            # time_ratio = torch.clamp(time_step / 0.05, 0.0, 1.0)
+            x += atom_feature_embedding  # * time_ratio.unsqueeze(-1)
 
         if time_step is not None:
             time_embed = self.time_step_encoder(time_step, clean_mask)
