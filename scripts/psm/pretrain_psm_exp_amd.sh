7d6138d7c6bb5e6b1d3506bfbddb5d11d90c0d43
peiranjin <peiranjin@microsoft.com>
2024-12-23 07:40:16 -0800

    save

--- scripts/psm/pretrain_psm_exp_amd.sh
+++ scripts/psm/pretrain_psm_exp_amd.sh
@@ -242,8 +242,8 @@ export MKL_THREADING_LAYER='GNU'
 
 echo -e "\n\n"
 echo "==================================MP==========================================="
-# [ -z "${n_gpu}" ] && n_gpu=$(rocm-smi | grep -c '^[0-9]') # new for MI250x
-[ -z "${n_gpu}" ] && n_gpu=$(nvidia-smi -L | wc -l)
+[ -z "${n_gpu}" ] && n_gpu=$(rocm-smi | grep -c '^[0-9]') # new for MI250x
+# [ -z "${n_gpu}" ] && n_gpu=$(nvidia-smi -L | wc -l)
 echo "n_gpu: ${n_gpu}"
 echo "MASTER_ADDR: ${MASTER_ADDR}"
 echo "MASTER_PORT: ${MASTER_PORT}"
