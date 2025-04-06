a5fab309a3f297ce901f018dbac047dec0a782f4
Xinwei Tai <v-xinweitai@microsoft.com>
2025-04-06 09:33:11 +0000

    save

--- sfm/modules/rotary_embedding.py
+++ sfm/modules/rotary_embedding.py
@@ -271,3 +271,113 @@ class SFMRotaryEmbedding(torch.nn.Module):
 
         q, k = self.apply_rotary_pos_emb(q, k, cos, sin)
         return q, k
+
+
+class SFM2DRotaryEmbedding(torch.nn.Module):
+    def __init__(
+        self,
+        dim,
+        max_position_embeddings=16384,
+        base=500000,
+        # base=10000,
+        device=None,
+        scaling_factor=1.0,
+    ):
+        super().__init__()
+        self.scaling_factor = scaling_factor
+        self.dim = dim
+        self.max_position_embeddings = max_position_embeddings
+        self.base = base
+        inv_freq = 1.0 / (
+            self.base
+            ** (
+                torch.arange(0, self.dim, 2, dtype=torch.int64).float().to(device)
+                / self.dim
+            )
+        )
+        self.register_buffer("inv_freq", inv_freq, persistent=True)
+        self.max_seq_len_cached = max_position_embeddings
+
+    def apply_rotary_pos_emb(self, q, k, cos, sin, position_ids=None, unsqueeze_dim=1):
+        """Applies Rotary Position Embedding to the query and key tensors.
+
+        Args:
+            q (`torch.Tensor`): The query tensor.
+            k (`torch.Tensor`): The key tensor.
+            cos (`torch.Tensor`): The cosine part of the rotary embedding.
+            sin (`torch.Tensor`): The sine part of the rotary embedding.
+            position_ids (`torch.Tensor`, *optional*):
+                Deprecated and unused.
+            unsqueeze_dim (`int`, *optional*, defaults to 1):
+                The 'unsqueeze_dim' argument specifies the dimension along which to unsqueeze cos[position_ids] and
+                sin[position_ids] so that they can be properly broadcasted to the dimensions of q and k. For example, note
+                that cos[position_ids] and sin[position_ids] have the shape [batch_size, seq_len, head_dim]. Then, if q and
+                k have the shape [batch_size, heads, seq_len, head_dim], then setting unsqueeze_dim=1 makes
+                cos[position_ids] and sin[position_ids] broadcastable to the shapes of q and k. Similarly, if q and k have
+                the shape [batch_size, seq_len, heads, head_dim], then set unsqueeze_dim=2.
+        Returns:
+            `tuple(torch.Tensor)` comprising of the query and key tensors rotated using the Rotary Position Embedding.
+        """
+        # cos = cos.unsqueeze(unsqueeze_dim)
+        # sin = sin.unsqueeze(unsqueeze_dim)
+        sLen = q.shape[-2]
+        q_embed = (q * cos[:, :sLen, :]) + (rotate_half(q) * sin[:, :sLen, :])
+        k_embed = (k * cos) + (rotate_half(k) * sin)
+        return q_embed, k_embed
+
+    def forward(self, q, k, v, position_ids=None, nhead=1):
+        """
+        Args:
+            q: [bs*num_attention_heads, tgt_dep, tgt_len, head_size]
+            k: [bs*num_attention_heads, tgt_dep, seq_len, head_size]
+            v: [bs*num_attention_heads, tgt_dep, seq_len, head_size]
+            position_ids: [bs, seq_len]
+        return:
+            q: [bs*num_attention_heads, tgt_dep, tgt_len, head_size]
+            k: [bs*num_attention_heads, tgt_dep, seq_len, head_size]
+        """
+        with torch.no_grad():
+            if position_ids is None:
+                position_ids = (
+                    torch.arange(v.shape[-2], device=q.device)
+                    .type_as(self.inv_freq)
+                    .unsqueeze(0)
+                    .repeat(v.shape[0], 1)
+                )
+            else:
+                max_seq_len = position_ids.size()[-1]
+                position_ids = (
+                    position_ids.unsqueeze(1)
+                    .repeat(1, nhead, 1)
+                    .reshape(-1, max_seq_len)
+                )
+
+            position_ids[..., :1]
+
+            position_ids[..., 1:]
+
+            # x: [bs, num_attention_heads, seq_len, head_size]
+            inv_freq_expanded = (
+                self.inv_freq[None, :, None]
+                .float()
+                .expand(position_ids.shape[0], -1, 1)
+            )
+            position_ids_expanded = position_ids[:, None, :].float()
+            device_type = v.device.type
+            device_type = (
+                device_type
+                if isinstance(device_type, str) and device_type != "mps"
+                else "cpu"
+            )
+            with torch.autocast(device_type=device_type, enabled=False):
+                freqs = (
+                    inv_freq_expanded.float() @ position_ids_expanded.float()
+                ).transpose(1, 2)
+                emb = torch.cat((freqs, freqs), dim=-1)
+                cos = emb.cos()
+                sin = emb.sin()
+
+            cos, sin = cos.to(dtype=v.dtype), sin.to(dtype=v.dtype)
+
+        q, k = self.apply_rotary_pos_emb(q, k, cos, sin)
+        return q, k
